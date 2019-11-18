56eb2b521c0c1d6e66dd0ded2bad669abc81bf54
Sphelelebk <56341798+Sphelelebk@users.noreply.github.com>
2019-11-18 08:59:54 +0200

    My first project

---
+++ mysite/polls/views.py
@@ -0,0 +1,25 @@
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.template import loader, RequestContext
+from .models import Question
+# Create your views here.
+
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+
+    context = {'latest_question': latest_questions}
+    return render(request, 'polls/index.html', context)
+
+    return HttpResponse(template.render(context))
+
+def detail(request, question_id):
+    return HttpResponse("This is the detail view of the question: % " % question_id)
+
+def results(request, question_id):
+    return HttpResponse("These are the result of the question: %s " % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vote on question: %s" % question_id)
+
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Careers No.23\PycharmProjects\Django-project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
