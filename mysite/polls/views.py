e5d4c4c0f50850cf5a5af217a84f4e6cbc7b7cab
Sphelelebk <56341798+Sphelelebk@users.noreply.github.com>
2019-11-18 09:46:39 +0200

    My first project

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,14 +8,14 @@ from .models import Question
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-
     context = {'latest_question': latest_questions}
     return render(request, 'polls/index.html', context)
 
     return HttpResponse(template.render(context))
 
 def detail(request, question_id):
-    return HttpResponse("This is the detail view of the question: % " % question_id)
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("These are the result of the question: %s " % question_id)
